57e4ba43b6c50023be3c8bb92cdd347be4f02387
epatneau <epatneau@gmail.com>
2024-11-13 11:55:23 -0600

    added code to python file + debugged/ran program

--- password_authentication.py
+++ password_authentication.py
@@ -6,17 +6,42 @@
 # them into a college application portal to view their acceptance status'
 
 # Set correct password and store to variable
-
+set_password = "W1ldW0lv3$"
 
 # Use while loop to ask for multiple inputs until correct
 # Ask user for password and store to local variable
-
+while True:
+    user_name = input("Enter username: ")
+    user_pass = input("Enter password: ")
 
 # Compare user password with set password
 # if correct break from loop
 # else keep asking
 # output correct or incorrect before restarting loop
-
+    if user_pass == set_password:
+        break
+    else:
+        print("Incorrect Password")
 
 # When correct password input print statement welcoming user to university 
 # application portal and let them view their application/acceptance status
+print("-----------------------")
+print("")
+print("Welcome to Common App!")
+print("")
+print("-----------------------")
+print("Purdue University")
+print("Application Status: Accepted")
+print("")
+print("University of Illinois")
+print("Application Status: Submitted")
+print("")
+print("Georgia Tech")
+print("Application Status: Submitted")
+print("")
+print("The Ohio State University")
+print("Application Status: Deferred")
+print("")
+print("Stanford University")
+print("Application Status: Accepted")
+print("")
