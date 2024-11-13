e7dbc98b0ba554ac8888d38119c6b876b6d46cf1
epatneau <epatneau@gmail.com>
2024-11-13 12:03:30 -0600

    added code to block user after 5 failed attempts

--- password_authentication.py
+++ password_authentication.py
@@ -11,37 +11,41 @@ set_password = "W1ldW0lv3$"
 # Use while loop to ask for multiple inputs until correct
 # Ask user for password and store to local variable
 while True:
-    user_name = input("Enter username: ")
-    user_pass = input("Enter password: ")
+    for i in range(5):
+        user_name = input("Enter username: ")
+        user_pass = input("Enter password: ")
 
 # Compare user password with set password
 # if correct break from loop
 # else keep asking
 # output correct or incorrect before restarting loop
-    if user_pass == set_password:
-        break
-    else:
-        print("Incorrect Password")
+        if user_pass == set_password:
+            break
+        else:
+            print("Incorrect Password or Username")
+    print("You have entered the wrong password too many times please wait 5 minutes to try again")
+    break
 
 # When correct password input print statement welcoming user to university 
 # application portal and let them view their application/acceptance status
-print("-----------------------")
-print("")
-print("Welcome to Common App!")
-print("")
-print("-----------------------")
-print("Purdue University")
-print("Application Status: Accepted")
-print("")
-print("University of Illinois")
-print("Application Status: Submitted")
-print("")
-print("Georgia Tech")
-print("Application Status: Submitted")
-print("")
-print("The Ohio State University")
-print("Application Status: Deferred")
-print("")
-print("Stanford University")
-print("Application Status: Accepted")
-print("")
+if user_pass == set_password:
+    print("-----------------------")
+    print("")
+    print("Welcome to Common App!")
+    print("")
+    print("-----------------------")
+    print("Purdue University")
+    print("Application Status: Accepted")
+    print("")
+    print("University of Illinois")
+    print("Application Status: Submitted")
+    print("")
+    print("Georgia Tech")
+    print("Application Status: Submitted")
+    print("")
+    print("The Ohio State University")
+    print("Application Status: Deferred")
+    print("")
+    print("Stanford University")
+    print("Application Status: Accepted")
+    print("")
