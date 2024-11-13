ce46de0ef1323571a981d7cc09361676556106bf
epatneau <epatneau@gmail.com>
2024-11-13 11:40:58 -0600

    comment outline for code + created python file

---
+++ password_authentication.py
@@ -0,0 +1,22 @@
+# Password Authentication Program
+# Alpha Version 1.0
+# Coder: Emma Patneau
+# Date: 2024-11-13
+# Purpose: Ask user for password and loop until correct and then let 
+# them into a college application portal to view their acceptance status'
+
+# Set correct password and store to variable
+
+
+# Use while loop to ask for multiple inputs until correct
+# Ask user for password and store to local variable
+
+
+# Compare user password with set password
+# if correct break from loop
+# else keep asking
+# output correct or incorrect before restarting loop
+
+
+# When correct password input print statement welcoming user to university 
+# application portal and let them view their application/acceptance status
